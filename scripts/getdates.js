d64c2894cae1aac72e8bf34537c2bde7a2e857ec
Nely15 <bruiz2@byupathway.edu>
2026-05-09 00:35:34 -0500

    errors fixed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 const currentYear = new Date().getFullYear();
 
-document.getElementById("currentYear").textContent = currentYear;
+document.getElementById("currentyear").textContent = currentYear;
 
-document.getElementById(lastModified).textContent = 'Last Modified: ${document.lastModified}';
+document.getElementById("lastModified").textContent = 'Last Modified: ${document.lastModified}';
