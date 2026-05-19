1a5dcc9ddd2765b28c05b4c7a5cbe538987a9b0e
Nely15 <bruiz2@byupathway.edu>
2026-05-19 17:20:03 -0500

    adjustments

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,13 +2,15 @@ const year = document.querySelector("#currentyear");
 year.textContent = new Date().getFullYear();
 
 const lastModified = document.querySelector("#lastModified");
-lastModified.textContent = 'Last Modified: ${document.lastModified}';
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
-//Hamburger Menu
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
 
-menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    menuButton.classList.toggle("open");
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
+    
 });
+
