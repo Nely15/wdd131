387581966cbe7d8dfe42567b33b26a3b5e44374e
Nely15 <bruiz2@byupathway.edu>
2026-05-24 21:25:57 -0500

    midifications

--- place-scripts/place.js
+++ place-scripts/place.js
@@ -1,6 +1,6 @@
 const currentYear = new Date().getFullYear();
 
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentYear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
