eb536cd2316c67459760570edb83471490ba7aed
Nely15 <bruiz2@byupathway.edu>
2026-06-08 21:13:03 -0500

    small fixes

--- product-review-form/scripts/review.js
+++ product-review-form/scripts/review.js
@@ -2,6 +2,11 @@
 Product Review Form
 Author: Blanca Ruiz
 */
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
 let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
 
